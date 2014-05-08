1f6cec53ee74644d55ff0c2c041f4f065f9128c1
Francesco Pandolfi <francesco.pandolfi@cern.ch>
2014-05-08 10:17:22 +0200

    first commit of analyzeEnergyScan.sh

---
+++ PositionAnalysis/analyzeEnergyScan.sh
@@ -0,0 +1,8 @@
+./positionAnalysis BTF_286_20140502-153528_beam
+./positionAnalysis BTF_287_20140502-155208_beam
+./positionAnalysis BTF_259_20140502-012847_beam
+./positionAnalysis BTF_308_20140503-002534_beam
+./positionAnalysis BTF_293_20140502-180258_beam
+./positionAnalysis BTF_314_20140503-024715_beam
+
+hadd -f PosAn_BTF_286_287.root PosAn_BTF_286_20140502-153528_beam.root PosAn_BTF_287_20140502-155208_beam.root
