cb1030b3a321b08bf51877ab0c7567c57542e60f
Francesco Pandolfi <francesco.pandolfi@cern.ch>
2014-05-23 18:27:59 +0200

    fitting -X +X with pol5 and seems to work

--- PositionAnalysis/calibrateCeF3Positioning.cpp
+++ PositionAnalysis/calibrateCeF3Positioning.cpp
@@ -21,8 +21,6 @@
 
 
 
-void fillSingleProfile( TProfile* hp, float diag, float r );
-
 
 
 
@@ -37,7 +35,7 @@ int main() {
 
 
 
-  int runNumber;
+  unsigned int runNumber;
   tree->SetBranchAddress( "runNumber", &runNumber );
   bool bgo_corr_ok;
   tree->SetBranchAddress( "bgo_corr_ok", &bgo_corr_ok );
@@ -94,10 +92,10 @@ int main() {
       gr_run_xyMap->SetPoint( gr_run_xyMap->GetN(), xBeam, yBeam );
     }
 
+    bool isScintFrontSingleEle = ( runNumber<100 ) ? (scintFront>120. && scintFront<700.) : (scintFront>500. && scintFront<2000.);
 
     if( !bgo_corr_ok ) continue;
-    if( !(scintFront>500. && scintFront<2000. ) ) continue;
-    //if( !(scintFront>500. && scintFront<2000. && nHodoClustersX==1 && nHodoClustersY==1) ) continue;
+    if( !isScintFrontSingleEle ) continue;
 
     float r02 = cef3[0]/cef3[2];
     float r13 = cef3[1]/cef3[3];
@@ -112,17 +110,10 @@ int main() {
     float diag13_Calo = d_Calo.X();
     float diag02_Calo = d_Calo.Y();
 
-    //hp_diag02_vs_dBeam->Fill(diag02_Beam, r02 );
-    //hp_diag13_vs_dBeam->Fill(diag13_Beam, r13 );
-    //hp_diag02_vs_dCalo->Fill(diag02_Calo, r02 );
-    //hp_diag13_vs_dCalo->Fill(diag13_Calo, r13 );
-    fillSingleProfile( hp_diag02_vs_dBeam, diag02_Beam, r02 );
-    fillSingleProfile( hp_diag13_vs_dBeam, diag13_Beam, r13 );
-    fillSingleProfile( hp_diag02_vs_dCalo, diag02_Calo, r02 );
-    fillSingleProfile( hp_diag13_vs_dCalo, diag13_Calo, r13 );
-    //if( 
-    //fillSingleProfile( hp_diag02_vs_dHodo, diag02_Hodo, r02 );
-    //fillSingleProfile( hp_diag13_vs_dHodo, diag13_Hodo, r13 );
+    hp_diag02_vs_dBeam->Fill(diag02_Beam, r02 );
+    hp_diag13_vs_dBeam->Fill(diag13_Beam, r13 );
+    hp_diag02_vs_dCalo->Fill(diag02_Calo, r02 );
+    hp_diag13_vs_dCalo->Fill(diag13_Calo, r13 );
 
 
   } // for entries
@@ -137,15 +128,24 @@ int main() {
   hp_diag13_vs_dCalo->SetLineColor(kGreen);
 
 
-  TF1* f1_d02 = new TF1("f1_d02", "pol3", -17., 0.);
-  f1_d02->SetLineColor( kBlack);
+  TF1* f1_d02 = new TF1("f1_d02", "pol5", -17., 17.);
+  f1_d02->SetLineColor( kGray);
   hp_diag02_vs_dCalo->Fit( f1_d02, "R" );
 
-  TF1* f1_d13 = new TF1("f1_d13", "pol3", 0., 17.);
+  TF1* f1_d13 = new TF1("f1_d13", "pol5", -17., 17.);
   f1_d13->SetLineColor(kRed);
-  hp_diag13_vs_dCalo->Fit( f1_d13, "R" );
+  hp_diag13_vs_dCalo->Fit( f1_d13, "R+" );
 
-  hp_diag02_vs_dCalo->Draw("");
+
+
+
+  TH2D* h2_axes = new TH2D("axes", "", 10, -20., 20., 10, 0., 3.);
+  h2_axes->Draw();
+
+  TLine* lineone = new TLine( -20., 1., 20., 1. );
+
+  hp_diag02_vs_dCalo->Draw("same");
+  lineone->Draw("same");
   hp_diag13_vs_dCalo->Draw("same");
   hp_diag02_vs_dBeam->Draw("same");
   hp_diag13_vs_dBeam->Draw("same");
@@ -170,6 +170,8 @@ int main() {
   ofs02 << f1_d02->GetParameter(1) << std::endl;
   ofs02 << f1_d02->GetParameter(2) << std::endl;
   ofs02 << f1_d02->GetParameter(3) << std::endl;
+  ofs02 << f1_d02->GetParameter(4) << std::endl;
+  ofs02 << f1_d02->GetParameter(5) << std::endl;
   ofs02.close();
   std::cout << "Diag02 fit parameters saved in: diag02_parameters.txt" << std::endl;
 
@@ -179,6 +181,8 @@ int main() {
   ofs13 << f1_d13->GetParameter(1) << std::endl;
   ofs13 << f1_d13->GetParameter(2) << std::endl;
   ofs13 << f1_d13->GetParameter(3) << std::endl;
+  ofs13 << f1_d13->GetParameter(4) << std::endl;
+  ofs13 << f1_d13->GetParameter(5) << std::endl;
   ofs13.close();
   std::cout << "Diag13 fit parameters saved in: diag13_parameters.txt" << std::endl;
 
@@ -186,13 +190,3 @@ int main() {
 
 }
 
-
-void fillSingleProfile( TProfile* hp, float diag, float r ) {
-
-  if( diag<0. ) 
-    hp->Fill( diag, 1./r );
-  else
-    hp->Fill( diag, r );
-
-
-}
