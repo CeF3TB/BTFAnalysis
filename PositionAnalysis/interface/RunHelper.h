60e23c140de46354e1d13d01288a5b80ff612810
michelif <micheli@pb-d-128-141-252-41.cern.ch>
2014-05-27 17:57:18 +0200

    switching from tstring to string

--- PositionAnalysis/interface/RunHelper.h
+++ PositionAnalysis/interface/RunHelper.h
@@ -3,7 +3,7 @@
 
 
 
-
+#include "TString.h"
 #include <string>
 
 class RunHelper {
@@ -12,7 +12,11 @@ class RunHelper {
 
   static void getBeamPosition( const std::string& runNamei, float& xBeam, float& yBeam );
   static void getBGOCoordinates( int iChannel, float& x , float& y );
+  void setCalibrationFiles(std::string tagName);
+  std::string getCalibrationFiles(bool isCef3);
 
+  std::string cef3CalibrationVersion_;
+  std::string bgoCalibrationVersion_;
 
 };
 
