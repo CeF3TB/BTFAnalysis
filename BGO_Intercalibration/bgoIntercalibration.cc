7ddbddd3fe452844b3608216c485cd39afe680ab
Clara Jorda <jorda@cern.ch>
2014-05-29 09:58:08 +0200

    moved to new txt format, add tgraph for mean value

--- BGO_Intercalibration/bgoIntercalibration.cc
+++ BGO_Intercalibration/bgoIntercalibration.cc
@@ -15,6 +15,9 @@
 #include "TVector2.h"
 #include "TLatex.h"
 #include "TMath.h"
+#include "TGraph.h"
+#include "TGraphErrors.h"
+#include "TLine.h"
 
 #include "../PositionAnalysis/fastDQM_CeF3_BTF.h"
 #include "../PositionAnalysis/interface/DrawTools.h"
@@ -31,21 +34,10 @@ using namespace RooFit ;
 
 #define BGO_CHANNELS 8
 enum {ch1, ch2, ch3, ch4, ch5, ch6, ch7, ch8};
+Double_t bgo_index    [BGO_CHANNELS] = {0.5,1.5,2.5,3.5,4.5,5.5,6.5,7.5};
+Double_t bgo_index_err[BGO_CHANNELS] = {0.,0.,0.,0.,0.,0.,0.,0.};
 TString BGOchannel[BGO_CHANNELS] = {"0", "1", "2", "3", "4", "5", "6", "7" };
 TString BGOnumber [BGO_CHANNELS] = {"1", "2", "3", "4", "5", "6", "7", "8" };
-/*
-std::string prefix = "analysisTrees/Reco_BTF_";
-std::string BGOrun[BGO_CHANNELS] = {
-  prefix+"BTF_530_20140505-014707_beam", // CHANNEL 0
-  prefix+"BTF_538_20140505-053339_beam", // CHANNEL 1 
-  prefix+"BTF_537_20140505-050526_beam", // CHANNEL 2 
-  prefix+"BTF_531_20140505-020502_beam", // CHANNEL 3 
-  prefix+"BTF_536_20140505-043018_beam", // CHANNEL 4 
-  prefix+"BTF_533_20140505-024712_beam", // CHANNEL 5 
-  prefix+"BTF_534_20140505-031520_beam", // CHANNEL 6  
-  prefix+"BTF_535_20140505-035012_beam", // CHANNEL 7 
-};
-*/
 
 std::string BGOrun[BGO_CHANNELS] = {
   "BTF_530_20140505-014707_beam", // CHANNEL 0
@@ -85,22 +77,26 @@ Double_t CrystalBall(Double_t *x, Double_t *par){
 
 int main(){
 
+  // -- create folder to store the txt files and plots
   std::string outputdir = "";
-  outputdir = "BGOInterCalibration_CB";
+  outputdir = "BGOInterCalibration_CB_28May_bis";
 
   std::string mkdir_command = "mkdir -p " + outputdir;
   system(mkdir_command.c_str());
   
+  // -- define histograms
   std::vector<TH1D*> rawHistos;
   std::vector<TH1D*> calibHistos;
   std::vector<float> calibConstants;
   float yMax = 0.;
   
+  // -- Raw distributions
   std::string meansRawFileName;
   meansRawFileName = outputdir + "/meansRaw_CB.txt";
   ofstream ofsmr(meansRawFileName.c_str());
 
-  double rawMean [BGO_CHANNELS];
+  double rawMean    [BGO_CHANNELS];
+  double rawMeanErr [BGO_CHANNELS];
   double channelRawMean    = 0.0;
   double channelRawMeanErr = 0.0;
   for( unsigned i=0; i<BGO_CHANNELS; ++i ) {
@@ -110,7 +106,8 @@ int main(){
     double mean = channelRawMean;
     double err  = channelRawMeanErr;
 
-    rawMean[i] = mean;
+    rawMean   [i] = mean;
+    rawMeanErr[i] = err;
     ofsmr << i << "\t " << mean << "\t +- " << err << std::endl;
 
     calibConstants.push_back(mean);
@@ -124,27 +121,32 @@ int main(){
   
   float calibAve = sumVector(calibConstants)/calibConstants.size();
   
+  // -- create txt with constants for intercalibration
   std::string constantsFileName;
-  constantsFileName = outputdir + "/constants_CB.txt";
+  constantsFileName = outputdir + "/constants_V0.txt";
   ofstream ofs(constantsFileName.c_str());
 
+  // -- Calibrated distributions
   std::string meansCalibFileName;
   meansCalibFileName = outputdir + "/meansCalib_CB.txt";
   ofstream ofsmc(meansCalibFileName.c_str());
 
-  double calibratedMean [BGO_CHANNELS];
+  double calibratedMean    [BGO_CHANNELS];
+  double calibratedMeanErr [BGO_CHANNELS];
   double channelCalibMean    = 0.0;
   double channelCalibMeanErr = 0.0;
   for( unsigned i=0; i<BGO_CHANNELS; ++i ) {
     float thisCalib = calibAve/calibConstants[i];
     calibHistos.push_back(fitSingleChannelBGO( outputdir, "calib", BGOrun[i], i, channelCalibMean, channelCalibMeanErr, thisCalib ));
    
-    ofs << i << "\t " << thisCalib << std::endl;
+    // ofs << i << "\t " << thisCalib << std::endl; // Old format, with channel
+    ofs << thisCalib << std::endl; // New VXX format, without channel
 
     double mean = channelCalibMean;
     double err  = channelCalibMeanErr;
 
-    calibratedMean[i] = mean;
+    calibratedMean   [i] = mean;
+    calibratedMeanErr[i] = err;
     ofsmc << i << "\t " << mean << "\t +- " << err << std::endl;
 
   }
@@ -160,6 +162,7 @@ int main(){
   std::cout << "Calibration average for BGO: " << calibAve << std::endl;
   std::cout << std::endl;
 
+  // -- estimate the RMS for giving the intercalibration estimate
   std::pair<float,float>        rawValues = getMeanRMS(        rawMean );
   std::pair<float,float> calibratedValues = getMeanRMS( calibratedMean );
 
@@ -172,6 +175,41 @@ int main(){
   std::cout << "" << std::endl;
   std::cout << " All: CALIBRATED (mean +- rms) = ( " << calibratedValues.first << " +- " << calibratedValues.second << " ) " << std::endl; 
   std::cout << " ---> Intercalibration: " << calIntercalibration << " % " << std::endl;
+  
+  TCanvas *cg = new TCanvas("mean graph RAW","mean graph RAW");
+  TGraphErrors *grRaw = new TGraphErrors(BGO_CHANNELS, bgo_index, rawMean, bgo_index_err, rawMeanErr);
+  TGraphErrors *grCal = new TGraphErrors(BGO_CHANNELS, bgo_index, calibratedMean, bgo_index_err, calibratedMeanErr);
+  cg->cd();
+
+  TH2D* h2_axes = new TH2D("axes", "", 8, 0., 8., 10, 2300., 3000. );
+  h2_axes->SetXTitle( "BGO channel" );
+  h2_axes->SetYTitle( "mean ADC counts" );
+  h2_axes->SetTitleOffset(1.3,"Y");
+  h2_axes->Draw("");
+  h2_axes->SetStats(0);
+
+  grRaw->SetMarkerStyle(20);
+  grRaw->SetMarkerSize(1.0);
+  grRaw->SetMarkerColor(kBlack);
+  grRaw->Draw("P");
+
+  grCal->SetMarkerStyle(21);
+  grCal->SetMarkerSize(1.0);
+  grCal->SetMarkerColor(kRed);
+  grCal->Draw("P");
+  
+  TLegend* legend = new TLegend( 0.55, 0.70, 0.85, 0.85 );
+  legend->SetFillColor(0);
+  legend->SetTextSize(0.035);
+  legend->SetTextFont(40);
+  legend->SetHeader("mean value from fit");
+  legend->AddEntry( grRaw, "Raw", "p");
+  legend->AddEntry( grCal, "Calibrated", "p");
+  legend->Draw("same");
+
+  cg->SaveAs( Form("%s/graphRaw.png", outputdir.c_str() ) );
+
+
 
 }
 
@@ -237,13 +275,19 @@ TH1D* fitSingleChannel( const std::string& outputdir, const std::string& name, c
   c1->cd();
   
   RooPlot* xframe = x.frame();
-  data.plotOn(xframe);
-  cball.plotOn(xframe,LineColor(kBlue));    
+  //data.plotOn(xframe);
+  //cball.plotOn(xframe,LineColor(kBlue));    
+
+  data.plotOn (xframe, Name("data"));
+  cball.plotOn(xframe, LineColor(kBlue), Name("cball"));    
+
   xframe->SetName("");
   xframe->SetTitle("");
   xframe->SetTitleOffset(1.4,"Y");
   xframe->Draw();
-  double chi2 = xframe->chiSquare();
+  // double chi2 = xframe->chiSquare();
+  double chi2 = xframe->chiSquare("cball", "data", 4);
+  double ndof = xframe->GetNbinsX();
   
   // -- draw BGO channel
   TPaveText* labelChan = new TPaveText( 0.2, 0.7, 0.3, 0.8, "brNDC");
@@ -251,7 +295,6 @@ TH1D* fitSingleChannel( const std::string& outputdir, const std::string& name, c
   labelChan->SetTextSize(0.038);
   labelChan->SetTextFont(42);
   labelChan->SetTextAlign(10);
-  // labelChan->AddText(name.c_str() );
   TString header = "";
   if (name.find("raw")  !=std::string::npos) header = "Raw";
   if (name.find("calib")!=std::string::npos) header = "Calibrated";
@@ -355,7 +398,7 @@ void drawHistos( const std::string& outputdir, std::vector<TH1D*> histos, const 
   double ymax_ = 0.0;
   for( unsigned i=0; i<histos.size(); ++i ) {
     // double thisMax = histos[i]->GetMaximum()/histos[i]->Integral();
-    double thisMax = histos[i]->GetMaximum()*2; // -- multiply by rebin size
+    double thisMax = histos[i]->GetMaximum()*1; // -- multiply by rebin size
     if( thisMax>ymax_ )
       ymax_ = thisMax;
   }
@@ -368,7 +411,7 @@ void drawHistos( const std::string& outputdir, std::vector<TH1D*> histos, const 
   h2_axes->SetStats(0);
 
   for( unsigned i=0; i<histos.size(); ++i ) {
-    histos[i]->Rebin(2);
+    // histos[i]->Rebin(2);
     histos[i]->SetLineColor( colors[i] );
     histos[i]->SetLineWidth( 2 );
     histos[i]->GetXaxis()->SetRangeUser(1000., 3500.);
