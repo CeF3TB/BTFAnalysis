ec87db67f3851b9f3b5c3654a197be1daa03a27e
Francesco Micheli <micheli@cmsrm-ui02.roma1.infn.it>
2014-06-05 14:03:04 +0200

    adding mkdir

--- PositionAnalysis/copyOnDcache.sh
+++ PositionAnalysis/copyOnDcache.sh
@@ -2,6 +2,8 @@
 
 VERSION=$1
 
+mkdir /pnfs/roma1.infn.it/data/cms/store/user/cmsdaqtb/analysisTrees/analysisTrees_$VERSION
+
 for i in /t3/users/cmsdaqtb/data/data/BTF/CeF3/runs/analysisTrees_$VERSION/*root; do
-    echo "dccp $i /pnfs/roma1.infn.it/data/cms/store/user/cmsdaqtb/analysisTrees/analysisTrees_V00"
+    dccp $i /pnfs/roma1.infn.it/data/cms/store/user/cmsdaqtb/analysisTrees/analysisTrees_$VERSION
 done
